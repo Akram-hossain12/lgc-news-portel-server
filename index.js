365297fee672fb2fe3636456338726993f428f66
Akram-hossain12 <akramhossain0502@gmail.com>
2023-03-06 10:23:44 +0600

    newsjson

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const cors =require('cors');
 const port =process.env.PORT || 5000;
 
 const catagoris = require('./data/catagoris.json');
+const news = require('./data/news.json');
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send('hello world')
+})
+app.get('/catagory/:id',(req,res)=>{
+    const id =req.params.id;
+    if(id==='08'){
+        res.send(news)
+    }
+    else{
+        const catagoryNews = news.filter(n=>n.category_id===id);
+        res.send(catagoryNews)
+    }
+})
+app.get('/news/:id',(req,res)=>{
+    const id =req.params.id;
+    const SelectedNews = news.find(n=>n._id===id);
+    res.send(SelectedNews)
+})
+app.get('/news',(req,res)=>{
+    res.send(news)
 })
 app.get('/news-catagoris',(req,res)=>{
     res.send(catagoris)
